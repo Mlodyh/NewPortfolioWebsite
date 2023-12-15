38bfb901aedc5127c3400df649118c342244c3bc
Hi i'm Łukasz <130846334+Mlodyh@users.noreply.github.com>
2023-12-15 15:45:08 +0100

    Add files via upload

---
+++ CV/script.js
@@ -0,0 +1,36 @@
+function updateCounter() {
+  const startDate = new Date("2023-06-10");
+  const currentDate = new Date();
+  const daysSinceStart = Math.floor(
+    (currentDate - startDate) / (1000 * 60 * 60 * 24)
+  );
+  document.getElementById(
+    "counter"
+  ).textContent = `${daysSinceStart} days ago.`;
+}
+
+updateCounter();
+
+setInterval(updateCounter, 86400000);
+
+// section
+
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll("nav a");
+
+  links.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: "smooth",
+        });
+      }
+    });
+  });
+});
